5db9d68b761f9a9f726b4acc5a2b4bf1c4350e4d
jupyter <irawanrafidzufar@gmail.com>
2026-04-19 19:21:34 +0700

    Second

--- resources/views/berita/index.blade.php
+++ resources/views/berita/index.blade.php
@@ -1,7 +1,8 @@
-<main class="bg-gray-300 h-screen">
+<main class="bg-gray-300 min-h-screen">
     <x-navbar/>
 
-    @foreach($data as $b)
+    <div class="min-h-screen">
+        @foreach($data as $b)
         <div class="mx-auto my-8 shadow-xl rounded-xl max-w-4xl bg-white border border-gray-500/50 px-6 py-4 flex flex-col gap-4">
             <a href="/berita/detail/{{ $b->id }}" class="font-bold text-blue-950 text-2xl hover:underline">{{ $b->judul }}</a>
             <p class="text-gray-900">
@@ -10,7 +11,9 @@
             </p>
             <a class="bg-red-600 text-gray-100 w-fit px-4 py-2 border border-red-600 rounded-xl shadow-xl hover:bg-gray-100 hover:text-red-600" href="/berita/delete/{{ $b->id }}">Hapus</a>
         </div>
-    @endforeach
+        @endforeach
+    </div>
+    
 
 </main>
 
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -11,6 +11,7 @@
                 Home
             </a>
         </ul>
+        @if (session('login'))
         <ul>
             <a href="/berita" class="btn btn-ghost flex my-auto text-gray-100 text-md font-bold py-2 px-6">
                 Dashboard
@@ -20,11 +21,18 @@
             <a href="/berita/create" class="btn btn-ghost flex my-auto text-gray-100 text-md font-bold py-2 px-6">
                 Tambah
             </a>
-        </ul>
+        </ul> 
+        @endif
         <ul>
+            @if(session('login'))
+            <a href="/logout">
+                <button class="btn btn-neutral">Logout</button>
+            </a>
+            @else
             <a href="/login">
                 <button class="btn btn-neutral">Login</button>
             </a>
+            @endif
         </ul>
     </li>
 </nav>
